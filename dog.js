fe09cb5fb8e692980777e52262a65af93cfaf3bb
Brody <broderickmcross@gmail.com>
2024-01-23 16:00:42 -0500

    Dogs array

---
+++ dog.js
@@ -0,0 +1,66 @@
+const dogs = [
+    {
+        name: 'german shepherd',
+        size: 'big',
+        activityLevel: 'very active',
+        friendliness: 'very friendly',
+        intelligence: 'very intelligent'
+    },
+    {
+        name: 'golden retriever',
+        size: 'big',
+        activityLevel: 'very active',
+        friendliness: 'very friendly',
+        intelligence: 'very intelligent'
+    },
+    {
+        name: 'beagle',
+        size: 'small',
+        activityLevel: 'somewhat active',
+        friendliness: 'very friendly',
+        intelligence: 'intelligent'
+    },
+    {
+        name: 'Boston Terrier',
+        size: 'small',
+        energyLevel: 'not very active',
+        friendliness: 'very friendly',
+        intelligence: 'intelligent'
+    },
+    {
+        name: 'Bulldog',
+        size: 'medium',
+        energyLevel: 'not very active',
+        friendliness: 'very friendly',
+        intelligence: 'intelligent'
+    },
+    {
+        name: 'Yorkshire Terrier',
+        size: 'small',
+        energyLevel: 'not very active',
+        friendliness: 'friendly',
+        intelligence: 'intelligent'
+    },
+    {
+        name: 'Boxer',
+        size: 'medium',
+        energyLevel: 'very active',
+        friendliness: 'somehat friendly',
+        intelligence: 'intelligent'
+    },
+    {
+        name: 'Mastiff',
+        size: 'big',
+        energyLevel: 'somewhat active',
+        friendliness: 'not as friendly',
+        intelligence: 'intelligent'
+    },
+    {
+        name: 'Bulldog',
+        size: 'medium',
+        energyLevel: 'not very active',
+        friendliness: 'very friendly',
+        intelligence: 'intelligent'
+    }
+]
+
